26cb52259b002dee1410b978ef39dc0bb6304cb8
Vivek Kumar <vvkkumar06@gmail.com>
2023-09-02 10:57:34 +0530

    13. Mutation

---
+++ src/db/categories.js
@@ -0,0 +1,43 @@
+export const categoriesData = [
+    {
+      id: "1",
+      name: "Electronics"
+    },
+    {
+      id: "2",
+      name: "Clothing"
+    },
+    {
+      id: "3",
+      name: "Home Appliances"
+    },
+    {
+      id: "4",
+      name: "Beauty and Personal Care"
+    },
+    {
+      id: "5",
+      name: "Books and Stationery"
+    },
+    {
+      id: "6",
+      name: "Sports and Outdoors"
+    },
+    {
+      id: "7",
+      name: "Furniture"
+    },
+    {
+      id: "8",
+      name: "Jewelry and Accessories"
+    },
+    {
+      id: "9",
+      name: "Toys and Games"
+    },
+    {
+      id: "10",
+      name: "Health and Wellness"
+    }
+  ];
+  
